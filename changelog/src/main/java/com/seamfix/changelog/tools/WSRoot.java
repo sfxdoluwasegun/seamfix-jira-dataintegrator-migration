75f378053efca423581455f10b86c5b5a7122666
DESKTOP-JEUJEIQ\Seamfix <mabikoye@seamfix.com>
2019-05-22 10:58:44 +0100

    #RD-350 - Made changes

---
+++ changelog/src/main/java/com/seamfix/changelog/tools/WSRoot.java
@@ -0,0 +1,8 @@
+package com.seamfix.changelog.tools;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath(value = "changelog")
+public class WSRoot extends Application {
+}
