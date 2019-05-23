a474ca37186a67bffaf81ba096873e76b699021c
DESKTOP-JEUJEIQ\Seamfix <mabikoye@seamfix.com>
2019-05-23 19:41:48 +0100

    #RD-357 - Added test endpoint

--- sprint/src/main/java/com/seamfix/sprint/rs/Main.java
+++ sprint/src/main/java/com/seamfix/sprint/rs/Main.java
@@ -3,19 +3,29 @@ package com.seamfix.sprint.rs;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
+import com.seamfix.sprint.model.QueryData;
 import com.seamfix.sprint.work.Workbook;
 
 
 @Path("/")
 public class Main {
 
+
+	@Inject
+	QueryData dataBean;
+	
 	@Inject 
 	Workbook workbook;
 	
 	@GET
-	@Path(value = "/log")
-	public String call() {
-		return workbook.getSprint(87, 532);
+	@Path("{projectID}/{sprintID}")
+	public Response call(@PathParam("projectID") int projectID, @PathParam("sprintID") int sprintID) {
+	    dataBean.init(projectID, sprintID );
+		workbook.getJSON();
+
+		return Response.ok().entity(dataBean.rsJSON()).type("application/json").build();
 	}
 }
