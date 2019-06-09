b6b28cb2d620a377eabd4dfa3f596877178dc606
Mary Abikoye <mabikoye@seamfix.com>
2019-06-09 22:48:36 +0100

    #RD-374 - Made changes

--- issue-keys/src/main/java/com/seamfix/IssueKey/model/Parent.java
+++ issue-keys/src/main/java/com/seamfix/IssueKey/model/Parent.java
@@ -8,4 +8,9 @@ import lombok.Setter;
 public class Parent {
 	private String id;
 	private String key;
+	private String storyPoint;
+	private String dateCreated;
+	private String dateModified;
+	private String reporter;
+	private String currentStatus;
 }
