5dceba321bafb3a3c772eb5bbc8ce30531010628
DESKTOP-JEUJEIQ\Seamfix <mabikoye@seamfix.com>
2019-07-01 18:55:37 +0100

    #RD-417 - Made changes

--- issue-keys/src/main/java/com/seamfix/IssueKey/tools/Resources.java
+++ issue-keys/src/main/java/com/seamfix/IssueKey/tools/Resources.java
@@ -4,14 +4,8 @@ import java.util.logging.Logger;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 public class Resources {
-
-    @Produces
-    @PersistenceContext
-    EntityManager entityManager;
 
     @Produces
     public Logger produceLog(InjectionPoint injectionPoint) {
