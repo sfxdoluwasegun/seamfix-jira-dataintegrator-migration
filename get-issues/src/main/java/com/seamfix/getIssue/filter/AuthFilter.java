0f2a5772976aa0d3b1adb7fc98d088d523f0756f
DESKTOP-JEUJEIQ\Seamfix <mabikoye@seamfix.com>
2019-07-02 11:39:54 +0100

    #RD-417 - Made changes

--- get-issues/src/main/java/com/seamfix/getIssue/filter/AuthFilter.java
+++ get-issues/src/main/java/com/seamfix/getIssue/filter/AuthFilter.java
@@ -1,29 +1,35 @@
 package com.seamfix.getIssue.filter;
 
 import java.io.IOException;
+import java.lang.System.Logger;
 
 import javax.inject.Inject;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
 
-import org.apache.http.HttpHeaders;
 
+import com.google.common.base.Strings;
 import com.seamfix.getIssue.model.QueryData;
-
 
 @Provider
 public class AuthFilter implements ContainerRequestFilter {
 	
-	
 	@Inject
 	QueryData dataBean;
+	
+	@Inject
+	Logger log;
 
 	@Override
-	public void filter(ContainerRequestContext requestContext) throws IOException {
-		String auth = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+	public void filter(ContainerRequestContext requestContext) throws WebApplicationException, IOException {
+		String auth = requestContext.getHeaderString("authorization");
+		if (Strings.isNullOrEmpty(auth)) {
+			throw new NotAuthorizedException("No authorization");
+		}
 		dataBean.setAuth(auth);
-		
 	}
 
 }
--- sprints/src/main/java/com/seamfix/sprints/filter/AuthFilter.java
+++ sprints/src/main/java/com/seamfix/sprints/filter/AuthFilter.java
@@ -1,26 +1,35 @@
 package com.seamfix.sprints.filter;
 
 import java.io.IOException;
+import java.lang.System.Logger;
 
 import javax.inject.Inject;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
 
+
+import com.google.common.base.Strings;
 import com.seamfix.sprints.model.QueryData;
 
 @Provider
 public class AuthFilter implements ContainerRequestFilter {
 	
-	
 	@Inject
 	QueryData dataBean;
+	
+	@Inject
+	Logger log;
 
 	@Override
-	public void filter(ContainerRequestContext requestContext) throws IOException {
+	public void filter(ContainerRequestContext requestContext) throws WebApplicationException, IOException {
 		String auth = requestContext.getHeaderString("authorization");
+		if (Strings.isNullOrEmpty(auth)) {
+			throw new NotAuthorizedException("No authorization");
+		}
 		dataBean.setAuth(auth);
-		
 	}
 
 }
