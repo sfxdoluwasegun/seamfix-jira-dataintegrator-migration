30cec21b47e7083a8dd8144db25bde013f5b07c9
DESKTOP-JEUJEIQ\Seamfix <mabikoye@seamfix.com>
2019-07-02 11:36:12 +0100

    #417 - Re-modified my authentication

--- issue-keys/src/main/java/com/seamfix/IssueKey/filter/AuthFilter.java
+++ issue-keys/src/main/java/com/seamfix/IssueKey/filter/AuthFilter.java
@@ -1,12 +1,17 @@
 package com.seamfix.IssueKey.filter;
 
 import java.io.IOException;
+import java.lang.System.Logger;
 
 import javax.inject.Inject;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
 
+
+import com.google.common.base.Strings;
 import com.seamfix.IssueKey.model.QueryData;
 
 @Provider
@@ -14,12 +19,17 @@ public class AuthFilter implements ContainerRequestFilter {
 	
 	@Inject
 	QueryData dataBean;
+	
+	@Inject
+	Logger log;
 
 	@Override
-	public void filter(ContainerRequestContext requestContext) throws IOException {
+	public void filter(ContainerRequestContext requestContext) throws WebApplicationException, IOException {
 		String auth = requestContext.getHeaderString("authorization");
+		if (Strings.isNullOrEmpty(auth)) {
+			throw new NotAuthorizedException("No authorization");
+		}
 		dataBean.setAuth(auth);
-		
 	}
 
 }
