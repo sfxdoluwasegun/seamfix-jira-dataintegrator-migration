243c771575be5f8d3c10488eec4bcab6d156a9e5
DESKTOP-JEUJEIQ\Seamfix <mabikoye@seamfix.com>
2019-07-02 11:40:46 +0100

    Revert "#RD-417 - Made changes"
    This reverts commit 0f2a5772976aa0d3b1adb7fc98d088d523f0756f.

--- changelog/src/main/java/com/seamfix/changelog/filter/AuthFilter.java
+++ changelog/src/main/java/com/seamfix/changelog/filter/AuthFilter.java
@@ -1,35 +1,28 @@
 package com.seamfix.changelog.filter;
 
 import java.io.IOException;
-import java.lang.System.Logger;
 
 import javax.inject.Inject;
-import javax.ws.rs.NotAuthorizedException;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
 
+import org.apache.http.HttpHeaders;
 
-import com.google.common.base.Strings;
 import com.seamfix.changelog.model.QueryData;
 
 @Provider
 public class AuthFilter implements ContainerRequestFilter {
 	
-	@Inject
-	QueryData dataBean;
 	
 	@Inject
-	Logger log;
+	QueryData dataBean;
 
 	@Override
-	public void filter(ContainerRequestContext requestContext) throws WebApplicationException, IOException {
-		String auth = requestContext.getHeaderString("authorization");
-		if (Strings.isNullOrEmpty(auth)) {
-			throw new NotAuthorizedException("No authorization");
-		}
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		String auth = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
 		dataBean.setAuth(auth);
+		
 	}
 
 }
--- sprints/src/main/java/com/seamfix/sprints/filter/AuthFilter.java
+++ sprints/src/main/java/com/seamfix/sprints/filter/AuthFilter.java
@@ -1,35 +1,26 @@
 package com.seamfix.sprints.filter;
 
 import java.io.IOException;
-import java.lang.System.Logger;
 
 import javax.inject.Inject;
-import javax.ws.rs.NotAuthorizedException;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
 
-
-import com.google.common.base.Strings;
 import com.seamfix.sprints.model.QueryData;
 
 @Provider
 public class AuthFilter implements ContainerRequestFilter {
 	
-	@Inject
-	QueryData dataBean;
 	
 	@Inject
-	Logger log;
+	QueryData dataBean;
 
 	@Override
-	public void filter(ContainerRequestContext requestContext) throws WebApplicationException, IOException {
+	public void filter(ContainerRequestContext requestContext) throws IOException {
 		String auth = requestContext.getHeaderString("authorization");
-		if (Strings.isNullOrEmpty(auth)) {
-			throw new NotAuthorizedException("No authorization");
-		}
 		dataBean.setAuth(auth);
+		
 	}
 
 }
