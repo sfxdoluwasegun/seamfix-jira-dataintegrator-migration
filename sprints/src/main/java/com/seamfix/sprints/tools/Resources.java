1e8c8bbab4f1646d3b35dd1288ae35a6aff4e56f
DESKTOP-JEUJEIQ\Seamfix <mabikoye@seamfix.com>
2019-07-22 16:38:55 +0100

    #RD-426 - Made changes

--- sprints/src/main/java/com/seamfix/sprints/tools/Resources.java
+++ sprints/src/main/java/com/seamfix/sprints/tools/Resources.java
@@ -5,19 +5,6 @@ import java.util.logging.Logger;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 
-/**
- * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
- * 
- * <p>
- * Example injection on a managed bean field:
- * </p>
- * 
- * <pre>
- * &#064;Inject
- * private EntityManager em;
- * </pre>
- */
-
 public class Resources {
 
 	    @Produces
